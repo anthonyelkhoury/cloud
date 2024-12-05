bafbdd6a53075204e3d7609201cddf5aaec92467
Anthony El Khoury <202110356@ua.edu.lb>
2024-12-05 02:10:19 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Task Scheduler</h1>
       </header>
+
+      <main>
+        <div className='task-form'>
+          <input type='text' id='task' placeholder='Enter task...' />
+          <select id='priority'>
+            <option value="top">Top Priority</option>
+            <option value="middle">Middle Priority</option>
+            <option value="low">Less Priority</option>
+          </select>
+          <input type='date' id='deadline' />
+          <button id='add-task'>Add Task</button>
+        </div>
+        <div class="task-list" id="task-list">
+
+        </div>
+      </main>
     </div>
   );
 }
